68a7db338ffbfc09fed9ff8898233d23d08c2124
Mohammad177 <m17768@gmail.com>
2021-11-18 10:12:04 +0330

    Rewrite Back to top button JavaScript file and edit related HTML.

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,18 +1,25 @@
 "use strict";
+
+{
+
+// Catch HTML element
+const backToTop = document.querySelector('#back-to-top');
+
+// Runs when a user scrolls
+window.onscroll = () => scroll();
+
 // When the user scrolls down 700px from the top of the document, show the button
-window.onscroll = function() {backToTop.scroll();};
-const backToTop = {
-  myButton: document.getElementById("back-to-top"), //Get the button
-  // Show and hide the button
-  scroll: function() {
-    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-      this.myButton.style.display = "block";
-    } else {
-      this.myButton.style.display = "none";
-    }
-  },
-  // When the user clicks on the button, scroll to the top of the document
-  top: function() {
-    window.scrollTo({top: 0, behavior: "smooth"});
+const scroll = () => {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
   }
 };
+
+// When the user clicks on the button, scroll to the top of the document
+backToTop.addEventListener('click', () => {
+  window.scrollTo({top: 0, behavior: "smooth"});
+});
+
+}
