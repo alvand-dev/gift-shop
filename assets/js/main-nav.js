bec3000616d743f9a576a506b8f8692f512c38c8
Mohammad177 <m17768@gmail.com>
2021-11-18 09:51:15 +0330

    Add comments to main-nav.js

--- assets/js/main-nav.js
+++ assets/js/main-nav.js
@@ -2,14 +2,17 @@
 
 {
 
+// Catch HTML elements
 const mainNav = document.querySelector('#main-nav');
 const open = document.querySelector('#open-main-nav-btn');
 const close = document.querySelector('#close-main-nav-btn');
 
+// Show the menu on click event
 open.addEventListener('click', () => {
   mainNav.style.width = "290px";
 });
 
+// Hide the menu on click event
 close.addEventListener('click', () => {
   mainNav.style.width = "0";
 });
