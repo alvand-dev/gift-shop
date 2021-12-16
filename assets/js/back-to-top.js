20333fd9a8c6528ffd1ad5638c2860a88e0e9234
Mohammad177 <m17768@gmail.com>
2021-12-16 09:37:25 +0330

    Rewrite back to top button JavaScript with a new method and reduce lines of code.

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,25 +1,20 @@
 "use strict";
 
 {
+  // Catch the HTML element
+  const backToTop = document.querySelector("#back-to-top");
 
-// Catch HTML element
-const backToTop = document.querySelector('#back-to-top');
+  // Show and hide scroll button
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 700) {
+      backToTop.style.display = "block";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
 
-// Runs when a user scrolls
-window.onscroll = () => scroll();
-
-// When the user scrolls down 700px from the top of the document, show the button
-const scroll = () => {
-  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-    backToTop.style.display = "block";
-  } else {
-    backToTop.style.display = "none";
-  }
-};
-
-// When the user clicks on the button, scroll to the top of the document
-backToTop.addEventListener('click', () => {
-  window.scrollTo({top: 0, behavior: "smooth"});
-});
-
+  // add functionality to scroll button
+  backToTop.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
 }
