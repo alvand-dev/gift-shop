85ca16e36502c63e67d7df00cfa7062b231dc90f
Mohammad177 <m17768@gmail.com>
2021-12-16 09:48:16 +0330

    Reform the codes with Prettier.

--- assets/js/main-nav.js
+++ assets/js/main-nav.js
@@ -1,20 +1,18 @@
 "use strict";
 
 {
+  // Catch HTML elements
+  const mainNav = document.querySelector("#main-nav");
+  const open = document.querySelector("#open-main-nav-btn");
+  const close = document.querySelector("#close-main-nav-btn");
 
-// Catch HTML elements
-const mainNav = document.querySelector('#main-nav');
-const open = document.querySelector('#open-main-nav-btn');
-const close = document.querySelector('#close-main-nav-btn');
+  // Show the menu on click event
+  open.addEventListener("click", () => {
+    mainNav.style.width = "290px";
+  });
 
-// Show the menu on click event
-open.addEventListener('click', () => {
-  mainNav.style.width = "290px";
-});
-
-// Hide the menu on click event
-close.addEventListener('click', () => {
-  mainNav.style.width = "0";
-});
-
+  // Hide the menu on click event
+  close.addEventListener("click", () => {
+    mainNav.style.width = "0";
+  });
 }
