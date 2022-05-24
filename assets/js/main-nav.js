36fe406d56912a3f87a819164f359e79f536c6c2
alvand1399 <alvand1399@gmail.com>
2022-05-24 16:43:11 +0430

    style: Reformat main-nav JS file with Pretier

--- assets/js/main-nav.js
+++ assets/js/main-nav.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
 {
   // Catch HTML elements
-  const mainNav = document.querySelector("#main-nav");
-  const open = document.querySelector("#open-main-nav-btn");
-  const close = document.querySelector("#close-main-nav-btn");
+  const mainNav = document.querySelector('#main-nav');
+  const open = document.querySelector('#open-main-nav-btn');
+  const close = document.querySelector('#close-main-nav-btn');
 
   // Show the menu on click event
-  open.addEventListener("click", () => {
-    mainNav.style.width = "290px";
+  open.addEventListener('click', () => {
+    mainNav.style.width = '290px';
   });
 
   // Hide the menu on click event
-  close.addEventListener("click", () => {
-    mainNav.style.width = "0";
+  close.addEventListener('click', () => {
+    mainNav.style.width = '0';
   });
 }
